3658a6213027f81975ae3e7f66d77a722e1a98a9
Alex Pritchard <alexbeet1234@gmail.com>
2024-01-06 12:50:49 +0000

    add clasrooms_controller - add validation to clasroom

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,7 @@
 class Classroom < ApplicationRecord
   belongs_to :teacher
+
+  validates :name, presence: true
+  validates :rows, :columns, presence: true,
+                             numericality: { only_integer: true, greater_than: 0 }
 end
