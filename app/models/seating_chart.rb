3913e1802ecf4d1f1f72812ebe186a5f59947f99
Alex Pritchard <alexbeet1234@gmail.com>
2024-01-06 20:44:38 +0000

    seating chart controller, model and controller - update classroom controler spec.

--- app/models/seating_chart.rb
+++ app/models/seating_chart.rb
@@ -19,9 +19,35 @@
 #  fk_rails_...  (classroom_id => classrooms.id)
 #
 class SeatingChart < ApplicationRecord
-  belongs_to :classroom, dependant: :destroy
+  belongs_to :classroom, dependent: :destroy
 
   # TODO: validate that the seating chart is not larger than the classroom to which it belongs.
   validates :rows, :columns, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :title, presence: true
+
+  validate :check_rows_and_columns
+
+  private
+
+  def check_rows_and_columns
+    check_rows && check_columns
+  end
+
+  def check_rows
+    if rows > classroom.rows
+      errors.add(:base, "Seating chart cannot have more rows than classroom.")
+      return false
+    end
+
+    true
+  end
+
+  def check_columns
+    if columns > classroom.columns
+      errors.add(:base, "Seating chart cannot have more columns than classroom.")
+      return false
+    end
+
+    true
+  end
 end
--- spec/controllers/seating_charts_controller_spec.rb
+++ spec/controllers/seating_charts_controller_spec.rb
@@ -2,24 +2,132 @@ require 'rails_helper'
 
 RSpec.describe SeatingChartsController, type: :controller do
   let(:teacher) { create(:teacher) }
-  let(:classroom) { create(:classroom, teacher: teacher) }
+  let(:classroom) { create(:classroom, teacher: teacher, rows: 10, columns: 10) }
   let(:seating_chart) { create(:seating_chart, classroom: classroom) }
+  let(:valid_attributes) { { title: 'chart 1', rows: classroom.rows, columns: classroom.columns, classroom_id: classroom.id } }
+  let(:invalid_attributes) { { title: '', rows: classroom.rows, columns: classroom.columns, classroom_id: classroom.id } }
+  let(:large_attributes) { { title: 'large chart', rows: (classroom.rows + 1), columns: (classroom.columns + 1), classroom_id: classroom.id } }
 
   before do
     sign_in teacher
   end
 
-  describe 'index' do
-    let!(:seating_charts) do
-      create_list(:seating_chart, 3, classroom: classroom)
-    end
-    
-    it 'returns a list of seating_charts' do
-      get :index
+  describe 'GET #new' do
+    it 'renders a successful response' do
+      get :new
       expect(response).to be_successful
-      expect(assigns(:seating_charts)).to match_array(seating_charts)
     end
   end
 
-  # Add tests for show, new, create, edit, update, destroy...
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      it 'creates a new SeatingChart' do
+        expect {
+          post :create, params: { seating_chart: valid_attributes }
+        }.to change(SeatingChart, :count).by(1)
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new SeatingChart' do
+        expect {
+          post :create, params: { seating_chart: invalid_attributes }
+        }.to change(SeatingChart, :count).by(0)
+      end
+
+      it 'renders the :new template' do
+        post :create, params: { seating_chart: invalid_attributes }
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context 'with seating chart larger than classroom' do
+      it 'does not create a new SeatingChart' do
+        expect {
+          post :create, params: { seating_chart: large_attributes }
+        }.to change(SeatingChart, :count).by(0)
+      end
+
+      it 'renders the :new template' do
+        post :create, params: { seating_chart: large_attributes }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'renders a successful response' do
+      get :edit
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      let(:new_attributes) { { title: 'updated chart' }}
+    
+      it 'renders a successful response' do
+        patch :update, params: { id: seating_chart.id, seating_chart: new_attributes }
+        expect(response).to be_successful
+      end
+
+      it 'updates seating_chart_title' do
+        patch :update, params: { id: seating_chart.id, seating_chart: new_attributes }
+        seating_chart.reload
+        expect(seating_chart.title).to eq('updated chart')
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'renders a successful response' do
+        patch :update, params: { id: seating_chart.id, seating_chart: invalid_attributes }
+        expect(response).to be_successful
+      end
+
+      it 'renders the :edit template' do
+        post :update, params: { id: seating_chart.id, seating_chart: invalid_attributes }
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    context 'with seating chart larger than classroom' do
+      let(:new_large_attributes) { { rows: (classroom.rows + 1) }}
+      let(:original_rows) { seating_chart.rows}
+
+      it 'does not update seating chart' do
+        patch :update, params: { id: seating_chart.id, seating_chart: new_large_attributes }
+        expect(seating_chart.rows).to eq(original_rows)
+      end
+
+      it 'renders the :edit template' do
+        patch :update, params: { id: seating_chart.id, seating_chart: new_large_attributes }
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'with successful conditions' do
+      it 'destroys the requested classroom' do
+        seating_chart = create(:seating_chart, classroom: classroom)
+        expect {
+          delete :destroy, params: { id: seating_chart.id }
+        }.to change(SeatingChart, :count).by(-1)
+      end
+    end
+    
+    context 'with unsuccessful conditions' do
+      let!(:seating_chart) { create(:seating_chart) }
+
+      before do
+        allow_any_instance_of(SeatingChart).to receive(:destroy).and_return(false)
+      end
+
+      it 'does not delete the classroom' do
+        expect {
+          delete :destroy, params: { id: seating_chart.id }
+        }.not_to change(SeatingChart, :count)
+      end
+    end
+  end
 end
